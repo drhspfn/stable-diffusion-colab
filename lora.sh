5b4e17cf909a15b129d608003f1d47d22fc7ca6e
drhspfn <63609847+drhspfn@users.noreply.github.com>
2023-06-09 18:33:03 +0300

    Update lora.sh

--- lora.sh
+++ lora.sh
@@ -4,4 +4,7 @@ cd /content/stable-diffusion-webui/models/Lora
 
 wget https://civitai.com/api/download/models/16677 --content-disposition
 wget https://civitai.com/api/download/models/23250 --content-disposition
-wget https://civitai.com/api/download/models/12873 --content-disposition
+
+# https://civitai.com/models/11363/spread-pussy
+#https://civitai.com/models/14171/cutegirlmix4
+#https://civitai.com/models/10844/perfect-pussy
