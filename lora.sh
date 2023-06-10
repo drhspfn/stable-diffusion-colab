e0717e2f6d39259f271685c98df473dd325b5574
drhspfn <63609847+drhspfn@users.noreply.github.com>
2023-06-10 19:01:22 +0300

    Update lora.sh

--- lora.sh
+++ lora.sh
@@ -5,6 +5,24 @@ cd /content/stable-diffusion-webui/models/Lora
 wget https://civitai.com/api/download/models/16677 --content-disposition
 wget https://civitai.com/api/download/models/23250 --content-disposition
 
-# https://civitai.com/models/11363/spread-pussy
-#https://civitai.com/models/14171/cutegirlmix4
-#https://civitai.com/models/10844/perfect-pussy
+# Spread pussy
+# CERVIX SPREAD PUSSY CLITORIS URETHRA
+wget https://civitai.com/api/download/models/25584 --content-disposition
+
+# Cute_girl_mix4
+# mix4
+wget https://civitai.com/api/download/models/16677 --content-disposition
+
+# Perfect pussy
+# <lora:newb_0.1:0.3>
+wget https://civitai.com/api/download/models/61391  --content-disposition
+
+
+# https://civitai.com/models/17379/female-masturbation-boob-fondling-and-fingering
+# masturbation, fingering, female_masturbation, grabbing_own_breast
+wget https://civitai.com/api/download/models/20546 --content-disposition
+
+
+# https://civitai.com/api/download/models/23250
+# <lora:breastinclassbetter_v141:0.5>
+wget https://civitai.com/api/download/models/23250 --content-disposition
