befb2e8fa120c7575765d7b3528acea8a15f4e6e
drhspfn <63609847+drhspfn@users.noreply.github.com>
2023-06-10 20:02:03 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,11 +3,21 @@ cd /content/stable-diffusion-webui/models/Stable-diffusion
 #unzip "/content/stable-diffusion-webui/models/Stable-diffusion/HD22 S.zip"
 #rm "/content/stable-diffusion-webui/models/Stable-diffusion/HD22 S.zip"
 
+
+# Hassaku (hentai model)
+wget https://civitai.com/api/download/models/62528 --content-disposition
+
+# MeinaHentai
+wget https://civitai.com/api/download/models/64046 --content-disposition
+
+# Grapefruit (hentai model)
+wget https://civitai.com/api/download/models/29179 --content-disposition
+
 # AbyssOrangeMix3
-wget https://civitai.com/api/download/models/17233 --content-disposition
+#wget https://civitai.com/api/download/models/17233 --content-disposition
 
 # Counterfeit-V3.0
-wget https://civitai.com/api/download/models/57618 --content-disposition
+#wget https://civitai.com/api/download/models/57618 --content-disposition
 
 # Perfect World
-wget https://civitai.com/api/download/models/65269 --content-disposition
+#wget https://civitai.com/api/download/models/65269 --content-disposition
