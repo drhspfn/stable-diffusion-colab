5ee4014a6ca1e0d8a1cdfc56d4d83f5cf6a4ec0e
drhspfn <63609847+drhspfn@users.noreply.github.com>
2023-06-10 19:56:04 +0300

    Update lora.sh

--- lora.sh
+++ lora.sh
@@ -5,6 +5,11 @@ cd /content/stable-diffusion-webui/models/Lora
 wget https://civitai.com/api/download/models/16677 --content-disposition
 wget https://civitai.com/api/download/models/23250 --content-disposition
 
+
+# https://civitai.com/models/20120/pov-missionary-vaginal-creampie-lora
+# MISSIONARY \ VAGINAL \ CLOSE UP \ CREAMPIE \ <SIZE> BREASTS\ EXPOSED BREASTS \ TANLINES \ STOCKINGS \ WEARING SHIRT \ SPREADING LEGS \ LEGS UP
+wget https://civitai.com/api/download/models/23898 --content-disposition
+
 # Spread pussy
 # CERVIX SPREAD PUSSY CLITORIS URETHRA
 wget https://civitai.com/api/download/models/25584 --content-disposition
