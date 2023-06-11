ef0d11f9dd3e73cd35b0cae8052d4d2161d985cf
drhspfn <63609847+drhspfn@users.noreply.github.com>
2023-06-11 19:19:31 +0300

    Create install_new.sh

---
+++ install_new.sh
@@ -0,0 +1,48 @@
+cd /content/stable-diffusion-webui/models/Stable-diffusion
+# Models
+# 1. Hassaku (hentai model)
+#    * [Civitai](https://civitai.com/models/2583/hassaku-hentai-model)
+wget https://civitai.com/api/download/models/62528 --content-disposition
+
+# Lora
+mkdir /content/stable-diffusion-webui/models/Lora
+cd /content/stable-diffusion-webui/models/Lora
+# 1. Paimon's paintings style (Genshin impact stickers)
+#    * `chibi / white background / simple background / thick outline`
+wget https://civitai.com/api/download/models/31475 --content-disposition
+
+# 2. [Tsumasaky] Sucrose - Genshin Impact LoRA
+#    * `sucrose /(genshin impact/)`
+wget https://civitai.com/api/download/models/6208 --content-disposition
+
+# 3. https://civitai.com/models/20120/pov-missionary-vaginal-creampie-lora
+#    * `MISSIONARY \ VAGINAL \ CLOSE UP \ CREAMPIE \ <SIZE> BREASTS\ EXPOSED BREASTS \ TANLINES \ STOCKINGS \ WEARING SHIRT \ SPREADING LEGS \ LEGS UP`
+wget https://civitai.com/api/download/models/23898 --content-disposition
+
+# 4. Spread pussy
+#    * `CERVIX \ SPREAD PUSSY \ CLITORIS \ URETHRA`
+wget https://civitai.com/api/download/models/25584 --content-disposition
+
+# 5. [Spread pussy](https://civitai.com/models/19669/real-spread-pussyexperimental)
+#    * `uncensored, realspreadpussy`
+wget https://civitai.com/api/download/models/30420 --content-disposition
+
+# 6. Cute_girl_mix4
+#    * `mix4`
+wget https://civitai.com/api/download/models/16677 --content-disposition
+
+# 7. Perfect pussy
+#    * `<lora:newb_0.1:0.3>`
+wget https://civitai.com/api/download/models/61391 --content-disposition
+
+# 8. [female-masturbation-boob-fondling-and-fingering](https://civitai.com/models/17379/female-masturbation-boob-fondling-and-fingering)
+#    * `masturbation, fingering, female_masturbation, grabbing_own_breast`
+wget https://civitai.com/api/download/models/20546 --content-disposition
+
+# 9. [breastinclassbetter_v141](https://civitai.com/models/9025/breastinclass-better-bodies)
+#    * `<lora:breastinclassbetter_v141:0.5>`
+wget https://civitai.com/api/download/models/23250 --content-disposition
+
+# 10. [Pussy sandwich](https://civitai.com/models/57836/pussy-sandwich)
+#     * `pussy sandwich`
+wget https://civitai.com/api/download/models/62276 --content-disposition
